b6bf9565dc585429634d5772397459d4c0393cbe
Anderson Patricio <andersongpatricio@hotmail.com>
2025-11-26 21:37:44 -0300

    first commit

---
+++ src/pages/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <button class="btn btn-primary">Primary</button>
+    <button class="btn btn-error">Erro</button>
+  </div>
+</template>
+
+<script setup>
+</script>
